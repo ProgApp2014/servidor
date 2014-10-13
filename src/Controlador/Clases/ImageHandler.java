1fcf52cf9f9d11fa74957d7f4f64a3bedb1886ec
Rodrigo Rossi <rodrigo.rossi.trabal@gmail.com>
2014-10-13 09:33:56 -0200

    Esta creada la clase para guardar imagenes desde un inputStream

---
+++ src/Controlador/Clases/ImageHandler.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador.Clases;
+ 
+import Controlador.Clases.ImageHanlderException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.sql.Timestamp;
+import java.util.Date; 
+
+/**
+ *
+ * @author rodro
+ */
+public class ImageHandler {
+ 
+    private static final String IMAGE_NAME = "images";
+    private static final String ROOT = System.getProperty("user.dir") + File.separator;
+    private static final String IMAGE_FOLDER = ROOT + IMAGE_NAME + File.separator;
+
+    public ImageHandler() {
+
+        Boolean success = (new File(ROOT + IMAGE_NAME)).mkdirs();
+        if (!success) {
+            System.out.println("Folder already exist");
+        }
+    }
+ 
+
+    /**
+     * 
+     * @param inputStream
+     * @return The full name of the created file 
+     * @throws ImageHanlderException if something go wrong 
+     */
+    public String saveInputStream(InputStream inputStream) throws ImageHanlderException {
+        try {
+            Date d = new Date();
+            Timestamp t;
+            t = new Timestamp(d.getTime());
+            t = new Timestamp(d.getTime());
+            String fileName = IMAGE_FOLDER + "Image" + t.toString();
+            File img = new File(fileName);
+            FileOutputStream ot = new FileOutputStream(img);
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            while ((read = inputStream.read(bytes)) != -1) {
+                ot.write(bytes, 0, read);
+            }
+            return fileName;
+        } catch (Exception e) {
+            throw new ImageHanlderException(e.getMessage() + " " + e.getStackTrace());
+        }
+    }
+}

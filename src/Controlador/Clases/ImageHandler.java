afc6f2d044711110719e70ac5ce7efaee89d175e
Rodrigo Rossi <rodrigo.rossi.trabal@gmail.com>
2014-10-21 00:15:44 -0200

    Merge branch 'master' of https://github.com/ProgApp2014/web
    Conflicts:
    	web/WEB-INF/perfil.jsp

--- src/Controlador/Clases/ImageHandler.java
+++ src/Controlador/Clases/ImageHandler.java
@@ -4,47 +4,74 @@
  * and open the template in the editor.
  */
 package Controlador.Clases;
- 
+
 import Controlador.Clases.ImageHanlderException;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Timestamp;
-import java.util.Date; 
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author rodro
  */
 public class ImageHandler {
- 
+
     private static final String IMAGE_NAME = "images";
     private static final String ROOT = System.getProperty("user.dir") + File.separator;
     private static final String IMAGE_FOLDER = ROOT + IMAGE_NAME + File.separator;
+    private static String dynamicFolder = null;
 
     public ImageHandler() {
 
-        Boolean success = (new File(ROOT + IMAGE_NAME)).mkdirs();
+        Boolean success = (new File(getStaticFolder())).mkdirs();
         if (!success) {
             System.out.println("Folder already exist");
         }
     }
- 
+
+    public static String getStaticFolder() {
+
+        if (dynamicFolder == null) {
+            try {
+                BufferedReader in = new BufferedReader(new FileReader("config.conf"));
+                dynamicFolder = in.readLine().trim();
+                System.out.println(dynamicFolder+" <<");
+                in.close();
+
+            } catch (FileNotFoundException ex) {
+                System.out.println(ex.getMessage()+" <<");
+                return IMAGE_FOLDER;
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage()+" <<");                
+                return IMAGE_FOLDER;
+            }
+        }
+        return dynamicFolder+IMAGE_NAME;
+
+    }
 
     /**
-     * 
+     *
      * @param inputStream
-     * @return The full name of the created file 
-     * @throws ImageHanlderException if something go wrong 
+     * @return The full name of the created file
+     * @throws ImageHanlderException if something go wrong
      */
-    public String saveInputStream(InputStream inputStream,String fileNameTmp) throws ImageHanlderException {
+    public String saveInputStream(InputStream inputStream, String fileNameTmp) throws ImageHanlderException {
         try {
             Date d = new Date();
             Timestamp t;
             t = new Timestamp(d.getTime());
             t = new Timestamp(d.getTime());
-            String fileName =  t.toString()+fileNameTmp;
-            File img = new File(IMAGE_FOLDER+fileName);
+            String fileName = t.toString() + fileNameTmp;
+            File img = new File(getStaticFolder() + fileName);
             FileOutputStream ot = new FileOutputStream(img);
             int read = 0;
             byte[] bytes = new byte[1024];

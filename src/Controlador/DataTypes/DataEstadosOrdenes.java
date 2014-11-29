f8e6f481e77c8ea4bd35cfc1a8b4b87f6c7caf53
Rodrigo Rossi <rodrigo.rossi.trabal@gmail.com>
2014-11-29 15:41:27 -0200

    Agrego funciones para que los estados sepan sus nombres

--- src/Controlador/DataTypes/DataEstadosOrdenes.java
+++ src/Controlador/DataTypes/DataEstadosOrdenes.java
@@ -1,4 +1,3 @@
-
 package Controlador.DataTypes;
 
 import Controlador.Clases.EstadosOrdenes;
@@ -9,58 +8,73 @@ import javax.xml.bind.annotation.XmlAccessorType;
 
 @XmlAccessorType(XmlAccessType.PUBLIC_MEMBER)
 public class DataEstadosOrdenes implements Serializable {
+
     private Integer id;
-    
+
     private DataOrdenCompra orden;
-    
+
     private Integer estado;
-    
+
     private Date fecha;
-    
-    public DataEstadosOrdenes(){}
-    
-    public DataEstadosOrdenes(EstadosOrdenes eo){
+
+    private String nombre;
+
+    public DataEstadosOrdenes() {
+    }
+
+    public DataEstadosOrdenes(EstadosOrdenes eo) {
         this.id = eo.getId();
-        this.orden = new DataOrdenCompra(eo.getOrden(),false);
+        this.orden = new DataOrdenCompra(eo.getOrden(), false);
         this.estado = eo.getEstado();
         this.fecha = eo.getFecha();
     }
-    
+
     public DataEstadosOrdenes(Integer id, DataOrdenCompra orden, Integer estado, Date fecha) {
         this.id = id;
         this.orden = orden;
         this.estado = estado;
         this.fecha = fecha;
     }
-    
+
     public Integer getId() {
         return id;
     }
-    
+
     public void setId(Integer id) {
         this.id = id;
     }
-    
+
     public DataOrdenCompra getOrden() {
         return orden;
     }
-    
+
+    public String getNombre() {
+        String res;
+        res = estado == 0 ? "Recibida" : estado == 1 ? "Preparada" : estado == 2 ? "Confirmada" : "Cancelada";
+        return res;
+
+    }
+
+    public void setNombre(String nombre) {
+        //this.nombre = nombre;
+    }
+
     public void setCliente(DataOrdenCompra orden) {
         this.orden = orden;
     }
-    
+
     public Integer getEstado() {
         return estado;
     }
-    
+
     public void setEstado(Integer estado) {
         this.estado = estado;
     }
-    
+
     public Date getFecha() {
         return fecha;
     }
-    
+
     public void setFecha(Date fecha) {
         this.fecha = fecha;
     }
